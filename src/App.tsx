ba43e07a2f190b80e8bb5423992526da34cc3e6e
NadyaCodes <nadyacorscadden@gmail.com>
2022-08-12 22:36:51 -0400

    remove dummy data and install bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -3,32 +3,49 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [locationSearch, setLocationSearch] = useState('Paris')
+  const [locationSearch, setLocationSearch] = useState('')
+  const [locations, setLocations] = useState<string[]>([])
+
+
+  //disables search button if field is blank
+  const disableSearch = locationSearch.trim() === '';
+
+  const addLocation = () => {
+    setLocations([locationSearch, ...locations]);
+    setLocationSearch('');
+  }
 
   return (
-    <div>
+    <div className="container">
       <h1>Weather App</h1>
       <div>
         <label>
           Add Location 
-          <input type="text" 
+          <input 
+            className="m1-1 mr-1" 
+            type="text" 
             value={locationSearch} 
             onChange={e => setLocationSearch(e.target.value)}
           />
         </label>
+        <button className='btn btn-primary'
+          onClick={addLocation} 
+          disabled={disableSearch}
+          >Search</button>
       </div>
 
       <div>
         <h2>Locations</h2>
-        <table>
+        <table className="table table-hover">
           <thead>
             <tr>
               <th>Name</th>
             </tr>
           </thead>
           <tbody>
-            <tr><td>Belfast</td></tr>
-            <tr><td>New York</td></tr>
+            {locations.map((location, index) => 
+              <tr key={index}><td>{location}</td></tr>
+            )}
           </tbody>
         </table>
       </div>
